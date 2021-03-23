a42fd2af18b0c4fe8f2a0efde9dcd1e5b1d256db
waterfairyliterate <waterfairyliterate@users.noreply.github.com>
2021-03-23 20:58:52 +0300

    product item update

--- src/pages/main_page/Body.js
+++ src/pages/main_page/Body.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import ProductCard from '../../components/product/product_card';
 import '../css/body.css';
+
 
 const Body = () => {
   return (
     <div className="container">
       Welcome to Rakoon E-Commerce
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
     </div>
   )
 }
