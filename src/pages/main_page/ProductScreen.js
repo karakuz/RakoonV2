fce19d7beb6f99e3ddca7c6e88e5e26fff9fdcb0
Yasemin Orhun <yasemin.orhun@ozu.edu.tr>
2021-04-04 18:21:47 +0300

    made product screen more responsive

--- src/pages/main_page/ProductScreen.js
+++ src/pages/main_page/ProductScreen.js
@@ -30,24 +30,24 @@ import '../css/bootstrap.min.css';
       <Link className='btn btn-light my-3' to = '/'>
         Go Back
       </Link>
-      <Row>
+      <Row style= {{margin: '2rem'}}>
         <Col md={6}>
-          <Image src={product.image} alt={product.name} />
+          <Image src={product.image} alt={product.name} style= {{position: 'relative', maxWidth: '100%', height: 'auto'}} />
         </Col>
-        <Col md={3}>
-          <ListGroup variant = 'flush'>
+        <Col md={3} >
+          <ListGroup variant = 'flush' >
             <ListGroup.Item><h3>{product.item_name}</h3> </ListGroup.Item>
             <ListGroup.Item>Price: ${product.price} </ListGroup.Item>
-            <ListGroup.Item>Description: ${product.description} </ListGroup.Item>
+            <ListGroup.Item>Description: {product.description} </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={3}>
-          <Card>
-          <ListGroup  variant ='flush'>
+          <Card style={{margin: '2rem'}}>
+          <ListGroup  variant ='flush' >
             <ListGroup.Item>
               <Row>
                 <Col>Price: </Col>
-                <Col><srong> ${product.price} </srong></Col>
+                <Col><srong> $ {product.price} </srong></Col>
               </Row>
             </ListGroup.Item>
             <ListGroup.Item>
