84c9772ee0f0dbd1a6b4e4a05614789422b5b943
erayozd <56823745+ErayOzdayioglu@users.noreply.github.com>
2021-04-06 22:17:07 +0300

    Added restfuıl routes

--- backend/config/rest.js
+++ backend/config/rest.js
@@ -3,81 +3,70 @@ const router = express.Router();
 const db = require('./database');
 const jwt = require('jsonwebtoken');
 
-router.post('/addToCart', async (req,res) => {
+router.post('/addToCart', async (req, res) => {
   const item = req.body.item;
   const sessionID = req.body.user;
   const user = jwt.verify(sessionID, 'shhhhh')
 
-  const result = await db.get(`SELECT user_id FROM users WHERE e_mail='${user.email}'`, {raw: true});
+  const result = await db.get(`SELECT user_id FROM users WHERE e_mail='${user.email}'`, { raw: true });
   const user_id = result[0].user_id;
 
   db.get(`REPLACE INTO users_cart(item_id,user_id) VALUES(
     ( SELECT item_id FROM items WHERE item_id=${item.id}),
     ( SELECT user_id FROM users WHERE user_id=${user_id})
-    )`, {raw: true});
+    )`, { raw: true });
 });
 
-router.post('/removeFromCart', async (req,res) => {
+router.post('/removeFromCart', async (req, res) => {
   const item = req.body.item;
   const sessionID = req.body.user;
   const user = jwt.verify(sessionID, 'shhhhh')
 
-  const result = await db.get(`SELECT user_id FROM users WHERE e_mail='${user.email}'`, {raw: true});
+  const result = await db.get(`SELECT user_id FROM users WHERE e_mail='${user.email}'`, { raw: true });
   const user_id = result[0].user_id;
 
-  db.get(`DELETE FROM users_cart WHERE item_id=${item.id} AND user_id=${user_id}`, {raw: true});
-  
+  db.get(`DELETE FROM users_cart WHERE item_id=${item.id} AND user_id=${user_id}`, { raw: true });
+
   res.send(true);
 });
 
-router.post('/products', async (req,res) => {
-  const result = await db.get(`SELECT * FROM items`, {raw: true});
 
-  res.send(result)
-});
-
-router.post('/getProduct', async (req,res) => {
-  const id = req.body.id;
-  const result = await db.get(`SELECT * FROM items WHERE item_id=${id}`);
-  res.send(result)
-});
-
-router.post('/getCartItems', async (req,res) => {
+router.post('/getCartItems', async (req, res) => {
   const sessionID = req.body.sessionID;
   const user = jwt.verify(sessionID, 'shhhhh')
 
   const result = await db.get(`SELECT * FROM users_cart WHERE user_id=${user.user_id}`);
-  
+
   let itemIDs = []
-  for(const index of result)
+  for (const index of result)
     itemIDs.push(index.item_id);
-  let products=[];
-  for(const itemID of itemIDs){
+  let products = [];
+  for (const itemID of itemIDs) {
     const result2 = await db.get(`SELECT * FROM items WHERE item_id=${itemID}`);
     products.push(result2[0]);
   }
-  if(products.length!==0) res.send(products)
+  if (products.length !== 0) res.send(products)
   else res.send("none")
 });
 
 //console.log(products);
-  /* for(let product of products){
-    const sql = `INSERT INTO items(item_name,price,description,image,category,store_id,rating_id,brand) VALUES(
-      '${product.name}',
-      ${product.price},
-      '${product.text}',
-      '${product.img}',
-      '${product.category}',
-      ${1},
-      NULL,
-      '${product.brand}'
-    )`;
-    await db.get(sql, {raw: true})
-    console.log(sql);
-  }
- */
-  /* await db.get(`REPLACE INTO users_bucket(item_id,user_id) VALUES(
-    SELECT user_id FROM users WHERE e_mail='${user.username}',
-    ${parseInt(item.id)})`, {raw: true}); */
+/* for(let product of products){
+  const sql = `INSERT INTO items(item_name,price,description,image,category,store_id,rating_id,brand) VALUES(
+    '${product.name}',
+    ${product.price},
+    '${product.text}',
+    '${product.img}',
+    '${product.category}',
+    ${1},
+    NULL,
+    '${product.brand}'
+  )`;
+  await db.get(sql, {raw: true})
+  console.log(sql);
+}
+*/
+/* await db.get(`REPLACE INTO users_bucket(item_id,user_id) VALUES(
+  SELECT user_id FROM users WHERE e_mail='${user.username}',
+  ${parseInt(item.id)})`, {raw: true}); */
 
 module.exports = router;
--- src/pages/main_page/ProductScreen.js
+++ src/pages/main_page/ProductScreen.js
@@ -5,59 +5,56 @@ import { Link, useParams } from 'react-router-dom'
 import Axios from 'axios';
 import '../css/bootstrap.min.css';
 
- const ProductScreen = () => {
+const ProductScreen = () => {
   const [product, setProduct] = useState([]);
   const { id } = useParams();
 
-  const getProducts = async() => {
+  const getProducts = async () => {
     const res = await Axios({
-      method: "POST",
-      data:{
-        id: id
-      },
+      method: "GET",
       withCredentials: true,
-      url: `http://localhost:4000/getProduct`,
+      url: `http://localhost:4000/product/${id}`,
     });
-    setProduct(res.data[0])
+    setProduct(res.data);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getProducts();
-  },[]);
-  
+  }, []);
+
   return (
     <>
-      <Link className='btn btn-light my-3' to = '/'>
+      <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
-      <Row style= {{margin: '2rem'}}>
+      <Row style={{ margin: '2rem' }}>
         <Col md={6}>
-          <Image src={product.image} alt={product.name} style= {{position: 'relative', maxWidth: '100%', height: 'auto'}} />
+          <Image src={product.image} alt={product.name} style={{ position: 'relative', maxWidth: '100%', height: 'auto' }} />
         </Col>
         <Col md={3} >
-          <ListGroup variant = 'flush' >
+          <ListGroup variant='flush' >
             <ListGroup.Item><h3>{product.item_name}</h3> </ListGroup.Item>
             <ListGroup.Item>Price: ${product.price} </ListGroup.Item>
             <ListGroup.Item>Description: {product.description} </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={3}>
-          <Card style={{margin: '2rem'}}>
-          <ListGroup  variant ='flush' >
-            <ListGroup.Item>
-              <Row>
-                <Col>Price: </Col>
-                <Col><srong> $ {product.price} </srong></Col>
-              </Row>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Row>
-                <Col>Status: </Col>
-                <Col> <srong> {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'} </srong> </Col>
-              </Row>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              {/* <Button className='btn-block'
+          <Card style={{ margin: '2rem' }}>
+            <ListGroup variant='flush' >
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price: </Col>
+                  <Col><srong> $ {product.price} </srong></Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status: </Col>
+                  <Col> <srong> {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'} </srong> </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {/* <Button className='btn-block'
                 type='button'
                 disabled={product.countInStock == 0} >
               Add To Cart
