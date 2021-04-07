6c6005e5416bf44cb74840940aa061adce0ede2a
erayozd <56823745+ErayOzdayioglu@users.noreply.github.com>
2021-04-07 15:21:58 +0300

    Invalid token error added to forgot

--- src/pages/forgot/reset.js
+++ src/pages/forgot/reset.js
@@ -6,31 +6,35 @@ import { useParams, useHistory } from 'react-router-dom';
 import Axios from "axios";
 
 const Reset = () => {
-    const { token } = useParams();
-    const [registerPassword, setRegisterPassword] = useState("");
-    const history = useHistory();
-    const PORT = process.env.PORT || 4000;
+  const { token } = useParams();
+  const [registerPassword, setRegisterPassword] = useState("");
+  const history = useHistory();
+  const PORT = process.env.PORT || 4000;
 
-    var url = `http://localhost:${PORT}/reset/${token}`
-    const submit = async(e) => {
-      e.preventDefault();
-      const res = await Axios.post(url, {
-        password: registerPassword
-      }).catch(err => console.log(`Error in reset.js: ${err}`));
-      if(res.data){
-        alert('Your password has been changed');
-        history.push('/login');
-      }
-    };
+  var url = `http://localhost:${PORT}/reset/${token}`
+  const submit = async (e) => {
+    e.preventDefault();
+    const res = await Axios.post(url, {
+      password: registerPassword
+    }).catch(err => console.log(`Error in reset.js: ${err}`));
+    if (res.data === "TokenInvalid") {
+      alert("Invalid Token");
+      history.push('/login');
+    }
+    else if (res.data) {
+      alert('Your password has been changed');
+      history.push('/login');
+    }
+  };
 
 
   return (
-    <Form style={{margin: "3em auto"}}>
+    <Form style={{ margin: "3em auto" }}>
       <Form.Group controlId="formBasicPassword">
         <Form.Label>New Password</Form.Label>
         <Form.Control type="password" placeholder="Password" onChange={e => setRegisterPassword(e.target.value)} />
       </Form.Group>
-      <Button variant="primary" type="submit" onClick={(e)=> submit(e)}>
+      <Button variant="primary" type="submit" onClick={(e) => submit(e)}>
         Submit
       </Button>
     </Form>
