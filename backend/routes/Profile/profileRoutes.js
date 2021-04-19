1d03c40ee773281cf53424167b9c1785c23fb507
erayozd <56823745+ErayOzdayioglu@users.noreply.github.com>
2021-04-19 23:58:09 +0300

    Profile personal info section

--- backend/routes/Profile/profileRoutes.js
+++ backend/routes/Profile/profileRoutes.js
@@ -5,21 +5,24 @@ const jwt = require("jsonwebtoken");
 
 
 router.put("/profile/update", async (req, res) => {
-  var userUpdate = req.body.user;
+  const sessionID = req.body.sessionID;
+  console.log(sessionID);
+  const user = await jwt.verify(sessionID, 'shhhhh');
+  const userUpdate = req.body.user;
   await User.update({ surname: userUpdate.surname, name: userUpdate.name, e_mail: userUpdate.e_mail }, {
     where: {
-      e_mail: userUpdate.oldemail
+      user_id: user.user_id
     }
   });
   res.send(true);
 });
 
 router.post("/profile/user", async (req, res) => {
-  const sessionID = req.body.user;
+  const sessionID = req.body.sessionID;
   console.log(sessionID);
   const user = await jwt.verify(sessionID, 'shhhhh');
-  console.log("23123123");
-  res.send(user);
+  var updatedUser = await User.findOne({ where: { user_id: user.user_id } });
+  res.send(updatedUser);
 })
 
 
--- src/pages/user_page/Profile.js
+++ src/pages/user_page/Profile.js
@@ -1,5 +1,5 @@
 import { Tab } from 'bootstrap';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { Card, Tabs } from 'react-bootstrap';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -8,29 +8,9 @@ import Axios from "axios";
 
 const Profile = (props) => {
   const sessionID = null || localStorage.getItem('sessionID') || sessionStorage.getItem('sessionID');
-  console.log(`In profile: `);
+
   const ref = useRef(true);
 
-  const getProfile = async () => {
-    const res = await Axios({
-      method: "POST",
-      data: {
-        sessionID: sessionID
-      },
-      withCredentials: true,
-      url: `http://localhost:4000/profile/user`,
-    });
-    ref.current = false;
-    console.log(res.data);
-  };
-  getProfile();
-
-  const userInfo = {
-    name: props.user.name,
-    surname: props.user.surname,
-    email: props.user.email,
-    oldemail: props.user.email
-  };
 
   const { token } = useParams();
   const [registerName, setRegisterName] = useState("");
@@ -40,13 +20,51 @@ const Profile = (props) => {
   const PORT = process.env.PORT || 4000;
 
   var url = `http://localhost:${PORT}/profile/update`
+
+  var userInfo = {
+    name: "",
+    surname: "",
+    email: "",
+    oldemail: ""
+  };
+  useEffect(() => {
+    const getProfile = async () => {
+      const res = await Axios({
+        method: "POST",
+        data: {
+          sessionID: sessionID
+        },
+        withCredentials: true,
+        url: `http://localhost:4000/profile/user`,
+      });
+      ref.current = false;
+      userInfo.name = res.data.name;
+      userInfo.surname = res.data.surname;
+      userInfo.email = res.data.e_mail;
+
+    };
+    getProfile().then(() => {
+      console.log(userInfo);
+      document.getElementById("name").value = userInfo.name;
+      document.getElementById("surname").value = userInfo.surname;
+      document.getElementById("email").value = userInfo.email;
+      setRegisterName(userInfo.name);
+      setRegisterSurname(userInfo.surname);
+      setRegisterEmail(userInfo.email);
+    });
+  }, []);
+
+
+
+
   const submit = async (e) => {
     e.preventDefault();
     userInfo.name = registerName;
     userInfo.surname = registerSurname;
     userInfo.email = registerEmail;
     const res = await Axios.put(url, {
-      user: userInfo
+      user: userInfo,
+      sessionID: sessionID
     }).catch(err => console.log(`Error in update.js: ${err}`));
     window.location.reload();
   };
@@ -65,12 +83,12 @@ const Profile = (props) => {
                 <Form style={{ margin: "2rem auto" }}>
                   <Form.Group controlId="formName">
                     <Form.Label style={{ paddingRight: "3rem" }}> Name: </Form.Label>
-                    <Form.Control type="name" placeholder={userInfo.name} onChange={e => setRegisterName(e.target.value)} />
-                    <Form.Control type="name" placeholder={userInfo.surname} onChange={e => setRegisterSurname(e.target.value)} />
+                    <Form.Control id="name" type="name" onChange={e => setRegisterName(e.target.value)} />
+                    <Form.Control id="surname" type="surname" onChange={e => setRegisterSurname(e.target.value)} />
                   </Form.Group>
                   <Form.Group controlId="formEmail">
                     <Form.Label> E-Mail Address: </Form.Label>
-                    <Form.Control type="email" placeholder={userInfo.email} onChange={e => setRegisterEmail(e.target.value)} />
+                    <Form.Control id="email" type="email" onChange={e => setRegisterEmail(e.target.value)} />
                   </Form.Group>
                   <Button variant="primary" type="submit" onClick={(e) => submit(e)} >
                     Update
