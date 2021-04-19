ed6aae272efd382fa49f6e0cad4328a36a452b16
erayozd <56823745+ErayOzdayioglu@users.noreply.github.com>
2021-04-19 22:18:06 +0300

    profile routes

--- backend/routes/Profile/profileRoutes.js
+++ backend/routes/Profile/profileRoutes.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const  User = require("../../models/user");
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
 
 
 router.put("/profile/update", async (req, res) => {
-    var userUpdate = req.body.user;
-    await User.update({ surname: userUpdate.surname, name: userUpdate.name, e_mail: userUpdate.e_mail}, {
-        where: {
-         user_id : userUpdate.id
-        }
-      });
-      res.send(true);
+  var userUpdate = req.body.user;
+  await User.update({ surname: userUpdate.surname, name: userUpdate.name, e_mail: userUpdate.e_mail }, {
+    where: {
+      e_mail: userUpdate.oldemail
+    }
+  });
+  res.send(true);
 });
+
+router.post("/profile/user", async (req, res) => {
+  const sessionID = req.body.user;
+  console.log(sessionID);
+  const user = await jwt.verify(sessionID, 'shhhhh');
+  console.log("23123123");
+  res.send(user);
+})
+
 
 module.exports = router;
--- src/pages/user_page/Profile.js
+++ src/pages/user_page/Profile.js
@@ -1,5 +1,5 @@
 import { Tab } from 'bootstrap';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Card, Tabs } from 'react-bootstrap';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -7,18 +7,35 @@ import { useParams, useHistory } from 'react-router-dom';
 import Axios from "axios";
 
 const Profile = (props) => {
+  const sessionID = null || localStorage.getItem('sessionID') || sessionStorage.getItem('sessionID');
   console.log(`In profile: `);
+  const ref = useRef(true);
+
+  const getProfile = async () => {
+    const res = await Axios({
+      method: "POST",
+      data: {
+        sessionID: sessionID
+      },
+      withCredentials: true,
+      url: `http://localhost:4000/profile/user`,
+    });
+    ref.current = false;
+    console.log(res.data);
+  };
+  getProfile();
+
   const userInfo = {
     name: props.user.name,
     surname: props.user.surname,
     email: props.user.email,
-    id: props.user.id
+    oldemail: props.user.email
   };
 
   const { token } = useParams();
-  const [registerName, setRegisterName ] = useState("");
-  const [registerSurname, setRegisterSurname ] = useState("");
-  const [registerEmail, setRegisterEmail ] = useState("");
+  const [registerName, setRegisterName] = useState("");
+  const [registerSurname, setRegisterSurname] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
   const history = useHistory();
   const PORT = process.env.PORT || 4000;
 
@@ -27,57 +44,57 @@ const Profile = (props) => {
     e.preventDefault();
     userInfo.name = registerName;
     userInfo.surname = registerSurname;
-    userInfo.email= registerEmail;
+    userInfo.email = registerEmail;
     const res = await Axios.put(url, {
       user: userInfo
     }).catch(err => console.log(`Error in update.js: ${err}`));
-    
+    window.location.reload();
   };
 
   return (
-    <div style={{ margin: '2rem', justifyContent: 'center'}}>
-      {      
-    <Tabs>
-    <Tab eventKey="account" title="Account"></Tab>
-    <Tab eventKey="privacy" title="Privacy"></Tab>
-    <Tab eventKey="orders" title="Orders"></Tab>
+    <div style={{ margin: '2rem', justifyContent: 'center' }}>
+      {
+        <Tabs>
+          <Tab eventKey="account" title="Account"></Tab>
+          <Tab eventKey="privacy" title="Privacy"></Tab>
+          <Tab eventKey="orders" title="Orders"></Tab>
 
-    <Tab.Content eventKey="account" style={{margin: '2rem '}}>
-       <Card>
-          <Card.Body>
-          <Form style={{ margin: "2rem auto"}}>
-            <Form.Group controlId="formName">
-             <Form.Label style={{ paddingRight: "3rem" }}> Name: </Form.Label>
-              <Form.Control type="name" placeholder={userInfo.name} onChange={e => setRegisterName(e.target.value)}/>
-              <Form.Control type="name" placeholder={userInfo.surname} onChange={e => setRegisterSurname(e.target.value)}/>
-            </Form.Group>
-            <Form.Group controlId="formEmail">
-             <Form.Label> E-Mail Address: </Form.Label>
-              <Form.Control type="email" placeholder={userInfo.email} onChange={e => setRegisterEmail(e.target.value)}/>
-            </Form.Group>
-            <Button variant="primary" type="submit" onClick={(e) => submit(e)} >
-              Update 
+          <Tab.Content eventKey="account" style={{ margin: '2rem ' }}>
+            <Card>
+              <Card.Body>
+                <Form style={{ margin: "2rem auto" }}>
+                  <Form.Group controlId="formName">
+                    <Form.Label style={{ paddingRight: "3rem" }}> Name: </Form.Label>
+                    <Form.Control type="name" placeholder={userInfo.name} onChange={e => setRegisterName(e.target.value)} />
+                    <Form.Control type="name" placeholder={userInfo.surname} onChange={e => setRegisterSurname(e.target.value)} />
+                  </Form.Group>
+                  <Form.Group controlId="formEmail">
+                    <Form.Label> E-Mail Address: </Form.Label>
+                    <Form.Control type="email" placeholder={userInfo.email} onChange={e => setRegisterEmail(e.target.value)} />
+                  </Form.Group>
+                  <Button variant="primary" type="submit" onClick={(e) => submit(e)} >
+                    Update
             </Button>
-          </Form>
-          </Card.Body>
-        </Card>
-    </Tab.Content>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Tab.Content>
 
-    <Tab.Content eventKey="privacy" style={{margin: '2rem'}}>
-       <Card>
-          <Card.Body> </Card.Body>
-        </Card>
-    </Tab.Content>
+          <Tab.Content eventKey="privacy" style={{ margin: '2rem' }}>
+            <Card>
+              <Card.Body> </Card.Body>
+            </Card>
+          </Tab.Content>
 
-    <Tab.Content eventKey="orders" style={{margin: '2rem'}}>
-       <Card>
-          <Card.Body>
-            <Card.Title>You have no orders yet. </Card.Title>
-            </Card.Body>
-        </Card>
-    </Tab.Content>
+          <Tab.Content eventKey="orders" style={{ margin: '2rem' }}>
+            <Card>
+              <Card.Body>
+                <Card.Title>You have no orders yet. </Card.Title>
+              </Card.Body>
+            </Card>
+          </Tab.Content>
 
-  </Tabs>
+        </Tabs>
       }
     </div>
   )
