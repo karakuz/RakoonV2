7b21703234572c7b25f38af3a4954f1662e7c715
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2021-05-01 22:16:53 +0300

    products model edit

--- backend/routes/Product/ProductRoutes.js
+++ backend/routes/Product/ProductRoutes.js
@@ -20,10 +20,10 @@ router.get("/product/:id", async (req, res) => {
 });
 
 router.get("/products", async (req, res) => {
-  var products = await db.get(`
+  /* var products = await db.get(`
     SELECT item_id, item_name, price, description, image, category, store_id, countInStock, brand, rate
-      FROM rakoon.items JOIN ratings ON items.item_id=ratings.item_id;`);
-  //var products = await db.get(`SELECT * FROM rakoon.items`);
+      FROM rakoon.items JOIN ratings ON items.item_id=ratings.item_id;`); */
+  var products = await db.get(`SELECT * FROM rakoon.items`);
   
   res.send(products);
 })
