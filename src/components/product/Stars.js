be0e4f27bc0dcd860c29bf332c2d12b8ab446856
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2021-05-01 21:30:45 +0300

    product card ratings

--- src/components/product/Stars.js
+++ src/components/product/Stars.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import '../../pages/css/star_rating.css';
+import Star from './Star';
 
-const Stars = () => {
+const Stars = (props) => {
+  const stars = ['5','4','3','2','1'];
+
   return (
-    <div class="rate">
-      <input type="radio" id="star5" name="rate" value="5" />
-      <label for="star5" title="text">5 stars</label>
-      <input type="radio" id="star4" name="rate" value="4" />
-      <label for="star4" title="text">4 stars</label>
-      <input type="radio" id="star3" name="rate" value="3" />
-      <label for="star3" title="text">3 stars</label>
-      <input type="radio" id="star2" name="rate" value="2" />
-      <label for="star2" title="text">2 stars</label>
-      <input type="radio" id="star1" name="rate" value="1" />
-      <label for="star1" title="text">1 star</label>
+    <div className="rate">
+      {
+        stars.map( (num) => {
+          return <Star num={num} highlited={props.rate}/>
+        })
+      }
+      {/* <input type="radio" name="rate" value="5"/>
+      <label for="star5" title="text" ></label>
+
+      <input type="radio" name="rate" value="4" />
+      <label for="star4" title="text"></label>
+      
+      <input type="radio" name="rate" value="3" />
+      <label for="star3" title="text" style={{color: "red"}}>3 stars</label>
+      
+      <input type="radio" name="rate" value="2" />
+      <label for="star2" title="text"></label>
+      
+      <input type="radio" name="rate" value="1" />
+      <label for="star1" title="text"></label> */}
     </div>
   )
 }
