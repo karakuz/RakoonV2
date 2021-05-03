4275f2fd6b4ea776ebf992d854eb44fb1b20cddd
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2021-05-04 02:20:13 +0300

    Customers can track order status

--- src/pages/user_page/Orders.js
+++ src/pages/user_page/Orders.js
@@ -2,22 +2,22 @@ import React, { useEffect } from 'react'
 import { Card } from 'react-bootstrap';
 import Axios from "axios";
 import ProfileNav from './ProfileNav';
+import Order from './Order';
 
-const Orders = (props) => {
-  const [orders, setOrders] = React.useState();
+const Orders = () => {
+  const [orders, setOrders] = React.useState([]);
 
   useEffect(() => {
     (async ()=>{
       const res = await Axios({
         method: "POST",
         data: {
-          sessionID: props.sessionID
+          sessionID: null || localStorage.getItem('sessionID') || sessionStorage.getItem('sessionID')
         },
         withCredentials: true,
         url: `http://localhost:4000/profile/orders`,
       });
       setOrders(res.data);
-      console.log(res.data);
     })();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -25,11 +25,17 @@ const Orders = (props) => {
   return (
     <div style={{ margin: '2rem', justifyContent: 'center' }}>
       <ProfileNav/>
-      <Card>
-        <Card.Body>
-          <Card.Title>You have no orders yet. </Card.Title>
-        </Card.Body>
-      </Card>
+      {
+        (orders.length === 0) ? 
+        <Card>
+          <Card.Body>
+            <Card.Title>You have no orders yet. </Card.Title>
+          </Card.Body>
+        </Card>
+        : Object.keys(orders).map((key) => {
+            return <Order orders={orders[key]}/>
+          })
+      }
     </div>
   )
 }
