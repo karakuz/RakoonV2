f16300b1ad25f8ab44f23f12134e291cf0162bd1
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2021-05-06 22:16:42 +0300

    wallet checkout loading

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -4,7 +4,7 @@ import Form from "react-bootstrap/Form";
 import Axios from "axios";
 import { Button, Col, Container } from 'react-bootstrap';
 import redX from '../css/redX.jpg';
-
+import Loading from '../cart/loading.gif';
 
 function numeral(number) {
   number = String(number);
@@ -17,7 +17,6 @@ function numeral(number) {
   number = number.split('.')[0] + '.' + number.split('.')[1].substring(0, 3);
   return number;
 }
-
 
 const Checkout = (props) => {
   const sessionID = null || localStorage.getItem('sessionID') || sessionStorage.getItem('sessionID');
@@ -57,6 +56,8 @@ const Checkout = (props) => {
   }; */
   // TO DO 
   const proceedPayment = async () => {
+    document.getElementById('cont').style.display = 'none';
+    document.getElementById('loading').style.display = 'block';
     const res = await Axios({
       method: "POST",
       data: {
@@ -68,7 +69,6 @@ const Checkout = (props) => {
     });
     console.log(res.data);
   }
-
 
   const getProducts = async () => {
     const res = await Axios({
@@ -96,124 +96,129 @@ const Checkout = (props) => {
   }, []);
 
   return (
-    <div >
-      <Link className='btn btn-light my-3' to='/cart'>
-        Go Back
-      </Link>
-      <div style={{ display: 'none', position: 'absolute', overflow: 'auto', width: '450px', boxShadow: '0 0 15px grey', background: 'white', top: '-90px', borderRadius: '10px' }} id='addressError'>
-        <img src={redX} alt="error" style={{ width: '70px', float: 'left' }} />
-        <div style={{ flexGrow: '1', marginTop: '3px' }}>
-          <span style={{ fontSize: '17px' }}>Please fill out the neccesary </span>
-          <div className="progress-bar-error">
-            <span className="progress-bar-inner"></span>
-          </div>
-        </div>
+    <div>
+      <div id="loading" style={{display: "none"}}>
+        <img src={Loading} alt='Loading...' style={{display: 'block', margin: "0 auto"}}/>
       </div>
-
-      <div style={{ display: 'none', position: 'absolute', overflow: 'auto', width: '450px', boxShadow: '0 0 15px grey', background: 'white', top: '-90px', borderRadius: '10px' }} id='paymentError'>
-        <img src={redX} alt="error" style={{ width: '70px', float: 'left' }} />
-        <div style={{ flexGrow: '1', marginTop: '3px' }}>
-          <span style={{ fontSize: '17px' }}>Please insert payment info!</span>
-          <div className="progress-bar-error">
-            <span className="progress-bar-inner"></span>
+      <div id="cont">
+        <Link className='btn btn-light my-3' to='/cart'>
+          Go Back
+        </Link>
+        <div style={{ display: 'none', position: 'absolute', overflow: 'auto', width: '450px', boxShadow: '0 0 15px grey', background: 'white', top: '-90px', borderRadius: '10px' }} id='addressError'>
+          <img src={redX} alt="error" style={{ width: '70px', float: 'left' }} />
+          <div style={{ flexGrow: '1', marginTop: '3px' }}>
+            <span style={{ fontSize: '17px' }}>Please fill out the neccesary </span>
+            <div className="progress-bar-error">
+              <span className="progress-bar-inner"></span>
+            </div>
           </div>
         </div>
+
+        <div style={{ display: 'none', position: 'absolute', overflow: 'auto', width: '450px', boxShadow: '0 0 15px grey', background: 'white', top: '-90px', borderRadius: '10px' }} id='paymentError'>
+          <img src={redX} alt="error" style={{ width: '70px', float: 'left' }} />
+          <div style={{ flexGrow: '1', marginTop: '3px' }}>
+            <span style={{ fontSize: '17px' }}>Please insert payment info!</span>
+            <div className="progress-bar-error">
+              <span className="progress-bar-inner"></span>
+            </div>
+          </div>
+        </div>
+
+        <h4 class="mb-5" style={{ textAlign: 'center' }}>Checkout</h4>
+        <Container style={{ display: 'block' }}>
+          <div class="row" >
+            <div class="col-md-4 order-md-2 mb-4">
+              <h4 class="d-flex justify-content-between align-items-center mb-3">
+                <span class="text-muted">Your cart</span>
+              </h4>
+              <ul class="list-group mb-3">
+                {
+                  products.map((product) => {
+                    return (
+                      <li class="list-group-item d-flex justify-content-between lh-condensed">
+                        <div>
+                          <h6 class="my-0">{product.item_name}</h6>
+                        </div>
+                        <span class="text-muted">${product.price}</span>
+                      </li>
+                    );
+                  })
+                }
+                <li class="list-group-item d-flex justify-content-between" >
+                  <div>
+                    <h6 class="my-0">Total: </h6>
+                  </div>
+                  <span>${products.reduce((a, v) => a = a + v.price, 0)}</span>
+
+                </li>
+                <div style={{ marginTop: '1rem' }}> <Button onClick={() => proceedPayment()}>Complete Payment</Button></div>
+              </ul>
+            </div>
+
+            <div class="col-md-8 order-md-1">
+              <Form style={{ width: 'auto' }} >
+                <h4 class="mb-4"> Address</h4>
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridCity">
+                    <Form.Label>First Name <span class="text-muted">*</span></Form.Label>
+                    <Form.Control id="firstname" type="firstname" /*onChange={e => setRegisterFirstname(e.target.value)}*/ />
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="formGridZip">
+                    <Form.Label>Surname <span class="text-muted">*</span></Form.Label>
+                    <Form.Control id="surname" type="surname" /*onChange={e => setRegisterSurname(e.target.value)}*/ />
+                  </Form.Group>
+                </Form.Row>
+
+                <Form.Group controlId="formGridAddress1">
+                  <Form.Label style={{ marginRight: '1rem' }}>Address <span class="text-muted">*</span></Form.Label>
+                  <Form.Control placeholder="1234 Main St" id="adress" type="adress" /*onChange={e => setRegisterAdress(e.target.value)}*/ />
+                </Form.Group>
+
+                <Form.Group controlId="formGridAddress2">
+                  <Form.Label style={{ marginRight: '1rem' }}>Address 2 <span class="text-muted">(Optional)</span></Form.Label>
+                  <Form.Control placeholder="Apartment, studio, or floor" id="adress2" type="adress2" /*onChange={e => setRegisterAdress2(e.target.value)}*/ />
+                </Form.Group>
+
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridZip">
+                    <Form.Label>Country <span class="text-muted">*</span> </Form.Label>
+                    <Form.Control id="country" type="country" /*onChange={e => setRegisterCountry(e.target.value)}*/ />
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="formGridCity">
+                    <Form.Label>City <span class="text-muted">*</span> </Form.Label>
+                    <Form.Control id="city" type="city" /*onChange={e => setRegisterCity(e.target.value)}*/ />
+                  </Form.Group>
+                  <Form.Group as={Col} controlId="formGridZip">
+                    <Form.Label>Zip <span class="text-muted">*</span></Form.Label>
+                    <Form.Control id="zipCode" type="zipCode" /*onChange={e => setRegisterZipCode(e.target.value)}*/ />
+                  </Form.Group>
+                </Form.Row>
+
+                <Form.Row>
+                  <Form.Group as={Col} controlId="formGridZip">
+                    <Form.Label >Phone Number <span class="text-muted">*</span></Form.Label>
+                    <Form.Control id="phoneNumber" type="phoneNumber" /*onChange={e => setRegisterPhoneNumber(e.target.value)}*/ />
+                  </Form.Group>
+
+                  <Form.Group as={Col} controlId="formGridCity">
+                    <Form.Label >E-Mail Address <span class="text-muted">(Optional)</span> </Form.Label>
+                    <Form.Control id="email" type="email" /*onChange={e => setRegisterEmail(e.target.value)}*/ />
+                  </Form.Group>
+                </Form.Row>
+                <Form.Text className="text-muted">
+                  * must be filled out
+                </Form.Text>
+              </Form>
+
+              <hr class="mb-4"></hr>
+              <Form.Group controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Save this information for next time" />
+              </Form.Group>
+              <hr class="mb-4"></hr>
+            </div>
+          </div>
+        </Container>
       </div>
-
-      <h4 class="mb-5" style={{ textAlign: 'center' }}>Checkout</h4>
-      <Container style={{ display: 'block' }}>
-        <div class="row" >
-          <div class="col-md-4 order-md-2 mb-4">
-            <h4 class="d-flex justify-content-between align-items-center mb-3">
-              <span class="text-muted">Your cart</span>
-            </h4>
-            <ul class="list-group mb-3">
-              {
-                products.map((product) => {
-                  return (
-                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                      <div>
-                        <h6 class="my-0">{product.item_name}</h6>
-                      </div>
-                      <span class="text-muted">${product.price}</span>
-                    </li>
-                  );
-                })
-              }
-              <li class="list-group-item d-flex justify-content-between" >
-                <div>
-                  <h6 class="my-0">Total: </h6>
-                </div>
-                <span>${products.reduce((a, v) => a = a + v.price, 0)}</span>
-
-              </li>
-              <div style={{ marginTop: '1rem' }}> <Button onClick={() => proceedPayment()}>Complete Payment</Button></div>
-            </ul>
-          </div>
-
-          <div class="col-md-8 order-md-1">
-            <Form style={{ width: 'auto' }} >
-              <h4 class="mb-4"> Address</h4>
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridCity">
-                  <Form.Label>First Name <span class="text-muted">*</span></Form.Label>
-                  <Form.Control id="firstname" type="firstname" /*onChange={e => setRegisterFirstname(e.target.value)}*/ />
-                </Form.Group>
-                <Form.Group as={Col} controlId="formGridZip">
-                  <Form.Label>Surname <span class="text-muted">*</span></Form.Label>
-                  <Form.Control id="surname" type="surname" /*onChange={e => setRegisterSurname(e.target.value)}*/ />
-                </Form.Group>
-              </Form.Row>
-
-              <Form.Group controlId="formGridAddress1">
-                <Form.Label style={{ marginRight: '1rem' }}>Address <span class="text-muted">*</span></Form.Label>
-                <Form.Control placeholder="1234 Main St" id="adress" type="adress" /*onChange={e => setRegisterAdress(e.target.value)}*/ />
-              </Form.Group>
-
-              <Form.Group controlId="formGridAddress2">
-                <Form.Label style={{ marginRight: '1rem' }}>Address 2 <span class="text-muted">(Optional)</span></Form.Label>
-                <Form.Control placeholder="Apartment, studio, or floor" id="adress2" type="adress2" /*onChange={e => setRegisterAdress2(e.target.value)}*/ />
-              </Form.Group>
-
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridZip">
-                  <Form.Label>Country <span class="text-muted">*</span> </Form.Label>
-                  <Form.Control id="country" type="country" /*onChange={e => setRegisterCountry(e.target.value)}*/ />
-                </Form.Group>
-                <Form.Group as={Col} controlId="formGridCity">
-                  <Form.Label>City <span class="text-muted">*</span> </Form.Label>
-                  <Form.Control id="city" type="city" /*onChange={e => setRegisterCity(e.target.value)}*/ />
-                </Form.Group>
-                <Form.Group as={Col} controlId="formGridZip">
-                  <Form.Label>Zip <span class="text-muted">*</span></Form.Label>
-                  <Form.Control id="zipCode" type="zipCode" /*onChange={e => setRegisterZipCode(e.target.value)}*/ />
-                </Form.Group>
-              </Form.Row>
-
-              <Form.Row>
-                <Form.Group as={Col} controlId="formGridZip">
-                  <Form.Label >Phone Number <span class="text-muted">*</span></Form.Label>
-                  <Form.Control id="phoneNumber" type="phoneNumber" /*onChange={e => setRegisterPhoneNumber(e.target.value)}*/ />
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridCity">
-                  <Form.Label >E-Mail Address <span class="text-muted">(Optional)</span> </Form.Label>
-                  <Form.Control id="email" type="email" /*onChange={e => setRegisterEmail(e.target.value)}*/ />
-                </Form.Group>
-              </Form.Row>
-              <Form.Text className="text-muted">
-                * must be filled out
-              </Form.Text>
-            </Form>
-
-            <hr class="mb-4"></hr>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Save this information for next time" />
-            </Form.Group>
-            <hr class="mb-4"></hr>
-          </div>
-        </div>
-      </Container>
     </div>
 
   )
--- src/pages/user_page/Wallet.js
+++ src/pages/user_page/Wallet.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { Card, Form, Button } from 'react-bootstrap';
 import Axios from "axios";
 import ProfileNav from './ProfileNav';
+import Loading from '../cart/loading.gif';
 
 const Wallet = (props) => {
     const sessionID = null || localStorage.getItem('sessionID') || sessionStorage.getItem('sessionID');
@@ -27,6 +28,9 @@ const Wallet = (props) => {
 
     const submit = async (e) => {
         e.preventDefault();
+        document.getElementById('cont').style.display = 'none';
+        document.getElementById('loading').style.display = 'block';
+        
         const token = inputToken;
         if (token > 10) {
             alert("Yavaş lan kaç tane alıyon");
@@ -43,37 +47,39 @@ const Wallet = (props) => {
             withCredentials: true,
             url: `http://localhost:4000/payment/transferFaucet`
         });
+        alert(`${token}RKN is added to your account`);
         window.location.reload();
-
-
     };
 
     return (
-        <div style={{ margin: '2rem', justifyContent: 'center' }}>
-            <ProfileNav />
-            <Card>
-                <Card.Body>
-                    <Card.Title>Wallet address: {walletAddress}  </Card.Title>
-                    <Card.Title>Balance: {walletBalance} RKN </Card.Title>
-                    <Form style={{ margin: "3em auto", position: "relative" }}>
-                        <Form.Group controlId="formBasicPassword" onChange={e => setInputToken(e.target.value)}>
-                            <Form.Label>Send Me RKN</Form.Label>
-                            <Form.Control type="number" />
-                        </Form.Group>
-                        <span style={{ fontSize: '16px', position: 'absolute', right: '0', color: 'red', display: 'none' }} className='InvalidCode'>Code is invalid</span>
-
-                        <div style={{ display: "flex" }}>
-                            <Button variant="primary" style={{ margin: "3em auto" }} type="submit" onClick={(e) => submit(e)} >
-                                Send
-                            </Button>
-
-                        </div>
-
-                    </Form>
-
-                </Card.Body>
-            </Card>
+      <div style={{ margin: '2rem', justifyContent: 'center' }}>
+        <div id="loading" style={{display: "none"}}>
+          <img src={Loading} alt='Loading...' style={{display: 'block', margin: "0 auto"}}/>
         </div>
+        <div id="cont">
+          <ProfileNav />
+          <Card>
+            <Card.Body>
+              <Card.Title>Wallet address: {walletAddress}  </Card.Title>
+              <Card.Title>Balance: {walletBalance} RKN </Card.Title>
+              <Form style={{ margin: "3em auto", position: "relative" }}>
+                <Form.Group controlId="formBasicPassword" onChange={e => setInputToken(e.target.value)}>
+                  <Form.Label>Send Me RKN</Form.Label>
+                  <Form.Control type="number" />
+                </Form.Group>
+                <span style={{ fontSize: '16px', position: 'absolute', right: '0', color: 'red', display: 'none' }} className='InvalidCode'>Code is invalid</span>
+
+                <div style={{ display: "flex" }}>
+                  <Button variant="primary" style={{ margin: "3em auto" }} type="submit" onClick={(e) => submit(e)} >
+                    Send
+                  </Button>
+                </div>
+              </Form>
+            </Card.Body>
+          </Card>
+        </div>
+        
+      </div>
     )
 }
 
