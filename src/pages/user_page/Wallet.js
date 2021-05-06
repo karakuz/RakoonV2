281940694170b01502bb228f213672ef5e8201c6
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2021-05-06 23:39:02 +0300

    wallet loading

--- src/pages/user_page/Wallet.js
+++ src/pages/user_page/Wallet.js
@@ -28,10 +28,13 @@ const Wallet = (props) => {
 
     const submit = async (e) => {
         e.preventDefault();
+        const token = inputToken;
+        if(token>10)
+          alert("Max token you can get once is 10");
+        
         document.getElementById('cont').style.display = 'none';
         document.getElementById('loading').style.display = 'block';
         
-        const token = inputToken;
         // eslint-disable-next-line no-unused-vars
         const res = await Axios({
             method: "POST",
@@ -43,37 +46,47 @@ const Wallet = (props) => {
             withCredentials: true,
             url: `http://localhost:4000/payment/transferFaucet`
         });
+        document.getElementById('loading').style.display = 'none';
         alert(`${token}RKN is added to your account`);
         window.location.reload();
     };
 
     return (
       <div style={{ margin: '2rem', justifyContent: 'center' }}>
-        <div id="loading" style={{display: "none"}}>
-          <img src={Loading} alt='Loading...' style={{display: 'block', margin: "0 auto"}}/>
-        </div>
-        <div id="cont">
           <ProfileNav />
-          <Card>
-            <Card.Body>
-              <Card.Title>Wallet address: {walletAddress}  </Card.Title>
-              <Card.Title>Balance: {walletBalance} RKN </Card.Title>
-              <Form style={{ margin: "3em auto", position: "relative" }}>
-                <Form.Group controlId="formBasicPassword" onChange={e => setInputToken(e.target.value)}>
-                  <Form.Label>Send Me RKN</Form.Label>
-                  <Form.Control type="number" max='10'/>
-                </Form.Group>
-                <span style={{ fontSize: '16px', position: 'absolute', right: '0', color: 'red', display: 'none' }} className='InvalidCode'>Code is invalid</span>
+        {
+          (walletAddress==="") ?
+            <div id="loading" style={{display: "block"}}>
+              <img src={Loading} alt='Loading...' style={{display: 'block', margin: "0 auto"}}/>
+            </div>
+          : 
+            <>
+              <div id="loading" style={{display: "none"}}>
+                <img src={Loading} alt='Loading...' style={{display: 'block', margin: "0 auto"}}/>
+              </div>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Wallet address: {walletAddress}  </Card.Title>
+                  <Card.Title>Balance: {walletBalance} RKN </Card.Title>
+                  <Form style={{ margin: "3em auto", position: "relative" }}>
+                    <Form.Group controlId="formBasicPassword" onChange={e => setInputToken(e.target.value)}>
+                      <Form.Label>Send Me RKN</Form.Label>
+                      <Form.Control type="number" max='10'/>
+                    </Form.Group>
+                    <span style={{ fontSize: '16px', position: 'absolute', right: '0', color: 'red', display: 'none' }} className='InvalidCode'>Code is invalid</span>
 
-                <div style={{ display: "flex" }}>
-                  <Button variant="primary" style={{ margin: "3em auto" }} type="submit" onClick={(e) => submit(e)} >
-                    Send
-                  </Button>
-                </div>
-              </Form>
-            </Card.Body>
-          </Card>
-        </div>
+                    <div style={{ display: "flex" }}>
+                      <Button variant="primary" style={{ margin: "3em auto" }} type="submit" onClick={(e) => submit(e)} >
+                        Send
+                      </Button>
+                    </div>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </>
+        }
+        
+          
         
       </div>
     )
