33b224f4d266bd6d44ba81a999ed425528cc127a
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2021-05-22 20:13:59 +0300

    campaign forms init

--- client/src/pages/store/StoreCampaigns.js
+++ client/src/pages/store/StoreCampaigns.js
@@ -5,10 +5,11 @@ const jwt = require("jsonwebtoken");
 
 const StoreCampaings = () => {
   const [campaigns, setCampaigns] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [items, setItems] = useState([]);
 
   const sessionID = null || localStorage.getItem('sessionID') || sessionStorage.getItem('sessionID');
   const user = jwt.verify(sessionID, 'shhhhh');
-  console.log(user);
 
   const getCampaigns = async () => {
     const res = await Axios({
@@ -18,26 +19,101 @@ const StoreCampaings = () => {
         role_id: user.role_id
       },
       withCredentials: true,
-      url: `http://localhost:4000/getCampaigns`,
+      url: `/getCampaigns`,
     });
     setCampaigns(res.data);
   }
 
+  const getItems = async () => {
+    const res = await Axios({
+      method: "POST",
+      data: {
+        user_id: user.user_id,
+        role_id: user.role_id
+      },
+      withCredentials: true,
+      url: `/getStoreItems`,
+    });
+    /* console.log(res.data); */
+    setItems(res.data);
+  }
+
   useEffect(() => {
     getCampaigns();
+    getItems();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  useEffect(() => {
+    let productCategories = new Map();
+    for(let item of items){
+      /* console.log(item);
+      console.log(item.category); */
+      if(productCategories.get(item.category) === undefined)
+        productCategories.set(item.category,[item])
+      else
+        productCategories.set(item.category, [...productCategories.get(item.category), item])
+    }
+    /* console.log(productCategories);
+    console.log(productCategories.keys());
+    console.log(Array.from(productCategories.keys())); */
+    setCategories(Array.from(productCategories.keys()));
+  }, [items])
+
 
   return (
     <div>
       <StoreNav user={user} />
 
-      <h5 style={{textAlign: "center"}}>Campaigns</h5>
-      <div style={{border: "1px solid red", width: "50%", margin: "40px auto", padding: "20px"}}>
-
+      <h3 style={{textAlign: "center"}}>Campaigns</h3>
+      <div style={{border: "1px solid red", width: "70%", margin: "40px auto", padding: "20px"}}>
+        {
+          (campaigns.length === 0) ? <p>Store does not have any campaign</p>: <span>campaign</span>
+        }
       </div>
-      <h5 style={{textAlign: "center"}}>Deploy Campaign</h5>
-      <div style={{border: "1px solid red", width: "50%", margin: "40px auto", padding: "20px"}}>
+      <h3 style={{textAlign: "center"}}>Deploy Campaign</h3>
+      <div style={{border: "1px solid red", width: "70%", margin: "40px auto", padding: "20px"}}>
+        
+        <h5>By Category</h5>
+        <div style={{display: "flex", justifyContent: "space-between", margin: "1.5rem 0 0 1rem"}}>
+          <label style={{margin: "auto 0", fontSize: "16px"}}>Category</label>
+          <select style={{width: "120px"}}>
+            {
+              categories.map( category => {
+                return <option value={category}>{category}</option>
+              })
+            }
+          </select>
+          <label style={{margin: "auto 0", fontSize: "16px"}}>Discount</label>
+          <input type="text" value="%" style={{width: "70px", marginLeft: "0"}}/>
 
+          <label style={{margin: "auto 0", fontSize: "16px"}}>Date By</label>
+          <input type="date" id="date"/>
+
+          <input type="button" value="Deploy" id="category"/>
+        </div>
+        
+        <h5 style={{marginTop: "2rem"}}>By Product</h5>
+        <div style={{display: "flex", justifyContent: "space-between", margin: "1.5rem 0 0 1rem"}}>
+          <label style={{margin: "auto 0", fontSize: "16px"}}>Product</label>
+          <select style={{width: "200px"}}>
+            {
+              items.map( product => {
+                return <option value={product.item_name}>{product.item_name}</option>
+              })
+            }
+          </select>
+
+          <label style={{margin: "auto 0", fontSize: "16px"}}>Discount</label>
+          <input type="text" value="%" style={{width: "70px", marginLeft: "0"}}/>
+
+          <label style={{margin: "auto 0", fontSize: "16px"}}>Date By</label>
+          <input type="date" id="date"/>
+
+          <input type="button" value="Deploy" id="category"/>
+
+        </div>
       </div>
     </div>
   )
