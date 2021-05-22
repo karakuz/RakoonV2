988b427e92906f4487841a133f63490f2d8b44bf
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2021-05-22 20:27:55 +0300

    campaign form edit

--- client/src/pages/store/StoreCampaigns.js
+++ client/src/pages/store/StoreCampaigns.js
@@ -11,6 +11,19 @@ const StoreCampaings = () => {
   const sessionID = null || localStorage.getItem('sessionID') || sessionStorage.getItem('sessionID');
   const user = jwt.verify(sessionID, 'shhhhh');
 
+  /* const [byCategoryDiscount, setByCategoryDiscount] = useState("");
+  const [byProductDiscount, setByProductDiscount] = useState("");
+  const [byPriceDiscount, setByPriceDiscount] = useState("");
+
+  const [byCategoryDateBy, setByCategoryDateBy] = useState("");
+  const [byProductDateBy, setByProductDateBy] = useState("");
+  const [byPriceDateBy, setByPriceDateBy] = useState("");
+
+  const [category, setCategory] = useState("");
+  const [product, setProduct] = useState("");
+  const [priceBetween1, setPriceBetween1] = useState("");
+  const [priceBetween2, setPriceBetween2] = useState(""); */
+
   const getCampaigns = async () => {
     const res = await Axios({
       method: "GET",
@@ -34,7 +47,6 @@ const StoreCampaings = () => {
       withCredentials: true,
       url: `/getStoreItems`,
     });
-    /* console.log(res.data); */
     setItems(res.data);
   }
 
@@ -48,32 +60,39 @@ const StoreCampaings = () => {
   useEffect(() => {
     let productCategories = new Map();
     for(let item of items){
-      /* console.log(item);
-      console.log(item.category); */
       if(productCategories.get(item.category) === undefined)
         productCategories.set(item.category,[item])
       else
         productCategories.set(item.category, [...productCategories.get(item.category), item])
     }
-    /* console.log(productCategories);
-    console.log(productCategories.keys());
-    console.log(Array.from(productCategories.keys())); */
+    
     setCategories(Array.from(productCategories.keys()));
   }, [items])
 
+  const categorySubmit = async () => {
+
+  }
+
+  const productSubmit = async () => {
+    
+  }
+
+  const priceSubmit = async () => {
+    
+  }
 
   return (
     <div>
       <StoreNav user={user} />
 
       <h3 style={{textAlign: "center"}}>Campaigns</h3>
-      <div style={{border: "1px solid red", width: "70%", margin: "40px auto", padding: "20px"}}>
+      <div style={{border: "1px solid red", width: "80%", margin: "40px auto", padding: "20px"}}>
         {
           (campaigns.length === 0) ? <p>Store does not have any campaign</p>: <span>campaign</span>
         }
       </div>
       <h3 style={{textAlign: "center"}}>Deploy Campaign</h3>
-      <div style={{border: "1px solid red", width: "70%", margin: "40px auto", padding: "20px"}}>
+      <div style={{border: "1px solid red", width: "80%", margin: "40px auto", padding: "20px"}}>
         
         <h5>By Category</h5>
         <div style={{display: "flex", justifyContent: "space-between", margin: "1.5rem 0 0 1rem"}}>
@@ -111,8 +130,24 @@ const StoreCampaings = () => {
           <label style={{margin: "auto 0", fontSize: "16px"}}>Date By</label>
           <input type="date" id="date"/>
 
-          <input type="button" value="Deploy" id="category"/>
+          <input type="button" value="Deploy" id="product"/>
 
+        </div>
+
+        <h5 style={{marginTop: "2rem"}}>By Price</h5>
+        <div style={{display: "flex", justifyContent: "space-between", margin: "1.5rem 0 0 1rem"}}>
+          <label style={{margin: "auto 0", fontSize: "16px"}}>Price Between</label>
+          <input type="number" style={{width: "80px"}}/>
+          <span style={{margin: "auto 0", fontSize: "16px"}}>and</span>
+          <input type="number" style={{width: "80px"}}/>
+                    
+          <label style={{margin: "auto 0", fontSize: "16px"}}>Discount</label>
+          <input type="text" value="%" style={{width: "70px", marginLeft: "0"}}/>
+
+          <label style={{margin: "auto 0", fontSize: "16px"}}>Date By</label>
+          <input type="date" id="date"/>
+
+          <input type="button" value="Deploy" id="price"/>
         </div>
       </div>
     </div>
