2502b1c1fb090c7dbd8d18bd43e1dffb6bac0370
erayozd <56823745+ErayOzdayioglu@users.noreply.github.com>
2021-05-26 01:53:10 +0300

    service worker and mobile

---
+++ client/public/serviceworker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "version-1";
+const self = this;
+
+// Install SW
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log("Opened Cache");
+
+                return cache.addAll(["index.html", "offline.html"]);
+            })
+    )
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request)
+                    .catch(() => caches.match("offline.html"));
+            })
+    )
+});
+
+
+self.addEventListener("activate", (event) => {
+    const cacheWhiteList = [];
+    cacheWhiteList.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if (!cacheWhiteList.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+})
