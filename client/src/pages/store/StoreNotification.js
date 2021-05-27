d0c136afa4373eb815403ec37dcc82a878070ea9
waterfairyliterate <waterfairyliterate@users.noreply.github.com>
2021-05-27 21:41:17 +0300

    notification

--- client/src/pages/store/StoreNotification.js
+++ client/src/pages/store/StoreNotification.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Card, Form, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import Axios from 'axios';
 import StoreNav from './StoreNav'
 const jwt = require("jsonwebtoken");
@@ -8,8 +9,11 @@ const StoreNotification = () => {
     const [message, setMessage] = useState("");
     const sessionID = null || localStorage.getItem('sessionID') || sessionStorage.getItem('sessionID');
     const user = jwt.verify(sessionID, 'shhhhh');
+    let history = useHistory();
     var url = process.env.NODE_ENV === "production" ? "https://rakoon-v-2-kbmgw.ondigitalocean.app" : "http://localhost:4000";
-    const setNotification = async () => {
+    
+    const setNotification = async (message, e) => {
+        e.preventDefault();
         console.log(message);
         const res = await Axios({
             method: "POST",
@@ -19,7 +23,9 @@ const StoreNotification = () => {
             withCredentials: true,
             url: `${url}/store/setNotification`,
         });
+        history.push('/store/notification');
         window.location.reload();
+        console.log(res);
     }
 
     useEffect(() => {
@@ -36,7 +42,7 @@ const StoreNotification = () => {
                         <Form.Label>Enter your notification here</Form.Label>
                         <Form.Control onChange={(e) => setMessage(e.target.value)}>
                         </Form.Control>
-                        <Button onClick={(e) => setNotification(message)}>Submit</Button>
+                        <Button type = "submit" onClick={(e) => setNotification(message, e)}>Submit</Button>
                     </Form>
                 </Card.Body>
             </Card>
