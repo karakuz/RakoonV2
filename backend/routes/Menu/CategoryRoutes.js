c5f347c11b2dce829b4862f04a6ec103c11a2f0a
Emre Karakuz <31669352+karakuz@users.noreply.github.com>
2021-06-02 22:11:03 +0300

    product stars on category and recommended pages is fixed

--- backend/routes/Menu/CategoryRoutes.js
+++ backend/routes/Menu/CategoryRoutes.js
@@ -3,23 +3,59 @@ const router = express.Router();
 const db = require('../../config/database.js');
 const Product = require("../../models/item");
 
-
 // Get all category names from db
 router.get("/categories", async (req, res) => {
     var categories = await Product.findAll({
         attributes: ["category"],
         group: "category"
     });
-    console.log(categories);
     res.send(categories);
 });
 
 router.get("/category/:name", async (req, res) => {
-    let products = await Product.findAll({ where: { category: req.params.name } });
-    if (products.length === 0) res.send("none");
-    else {
-        res.send(products);
-    }
+  /* let products = await Product.findAll({ where: { category: req.params.name } });
+  if (products.length === 0) res.send("none");
+  else {
+      res.send(products);
+  } */
+
+  let all_category_products = await db.get(`
+  SELECT 
+	  items.*,
+    store.store_name,
+    campaign_items.old_price, 
+    0 as rate FROM items
+  LEFT JOIN store 
+  ON store.store_id = items.store_id 
+  LEFT JOIN campaign_items 
+  ON campaign_items.item_id = items.item_id
+  WHERE category = '${req.params.name}'
+  ORDER BY item_id;
+  `);
+
+  let map = new Map();
+  for(let product of all_category_products)
+    map.set(product.item_id, {...product});
+  
+  const rated_products = await db.get(`
+    SELECT item_id, store.store_name, item_name, price, image, AVG(rate) as rate FROM 
+      (SELECT 
+        items.*,
+        rate,
+        is_verified
+      FROM rakoon.items 
+      RIGHT JOIN ratings 
+      ON items.item_id=ratings.item_id
+      ) AS J
+      JOIN store ON store.store_id = J.store_id
+      WHERE is_verified = 1 AND category = '${req.params.name}'
+    `);
+  
+  for(let product of rated_products)
+    map.set(product.item_id, {...product, old_price: map.get(product.item_id).old_price});
+  
+  res.send(Array.from(map.values()));
+  
 })
 
 module.exports = router;
--- backend/routes/Product/ProductRoutes.js
+++ backend/routes/Product/ProductRoutes.js
@@ -49,14 +49,61 @@ router.get("/products", async (req, res) => {
 })
 
 router.get("/getRecommendation/:id", async (req, res) => {
+  /* var product = await Product.findOne({ where: { item_id: req.params.id } });
   
-  var product = await Product.findOne({ where: { item_id: req.params.id } });
-  console.log(product);
-  let similar_products = await db.get(`SELECT * FROM items WHERE items.category = '${product.category}' ORDER BY RAND() LIMIT 4`);
+  let similar_products = await db.get(`
+  SELECT * 
+  FROM items 
+  WHERE items.category = '${product.category}' 
+  ORDER BY RAND() 
+  LIMIT 4`);
+
   let map = new Map();
-  console.log(similar_products);
   for(let product of similar_products)
+    map.set(product.item_id, {...product}); */
+
+  const category = await db.get(`
+    SELECT 
+      category
+    FROM items WHERE item_id = ${req.params.id}
+  `);
+
+  let all_category_products = await db.get(`
+  SELECT 
+	  items.*,
+    store.store_name,
+    campaign_items.old_price, 
+    0 as rate FROM items
+  LEFT JOIN store 
+  ON store.store_id = items.store_id 
+  LEFT JOIN campaign_items 
+  ON campaign_items.item_id = items.item_id
+  WHERE category = '${category[0].category}'
+  ORDER BY RAND()
+  LIMIT 4
+  `);
+
+  let map = new Map();
+  for(let product of all_category_products)
     map.set(product.item_id, {...product});
+  
+  const rated_products = await db.get(`
+    SELECT item_id, store.store_name, item_name, price, image, AVG(rate) as rate FROM 
+      (SELECT 
+        items.*,
+        rate,
+        is_verified
+      FROM rakoon.items 
+      RIGHT JOIN ratings 
+      ON items.item_id=ratings.item_id
+      ) AS J
+      JOIN store ON store.store_id = J.store_id
+      WHERE is_verified = 1 AND category = '${category[0].category}'
+    `);
+  
+  for(let product of rated_products)
+    if(map.get(product.item_id) !== undefined)
+      map.set(product.item_id, {...product, old_price: map.get(product.item_id).old_price});
 
   res.send(Array.from(map.values()));
 })
